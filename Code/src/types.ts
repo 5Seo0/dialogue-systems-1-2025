3f375b9a2398a58e2ab012da696dabd0e1f13fe2
5Seo0 <seoyoung0405@gmail.com>
2026-01-20 22:50:31 +0100

    Lab 4 working version

--- Code/src/types.ts
+++ Code/src/types.ts
@@ -1,26 +1,13 @@
 import { Hypothesis, SpeechStateExternalEvent } from "speechstate";
 import { AnyActorRef } from "xstate";
 
-/*export interface DMContext {
-  spstRef: AnyActorRef;
-  lastResult: Hypothesis[] | null;
-  lastResultInterpretation: string | null
-}*/
-
 export interface DMContext {
   spstRef: AnyActorRef;
-  lastResult: Hypothesis[] | null;
-  playerPos: {
-    x: number;
-    y: number;
-  };
-  speech: string | null;
-  moveDirection?: string | null;
-  showMaze? : boolean | null;
-  confirm? : boolean | null;
-  inDarkZone: boolean;
-  awaitingWhereAmI: boolean;
-  promptIndex:number;
+  lastResult: any | null;
+  /*lastResultInterpretation?: string | null;*/
+    day: string | null;
+    time: string | null;
+    personName: string | null;
 }
 
 export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" };
